ba52b4870da3e5bf5de61ed449d721bd34448756
Brian-Zaniolo <bzaniolo83@gmail.com>
2024-04-11 11:50:55 +0200

    Add Redux and React-Redux dependencies, update CSS styling, and implement post creation and deletion functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { nanoid } from "@reduxjs/toolkit";
 
-function App() {
+const App = () => {
+  const [inputValues, setInputValues] = useState({ title: "", content: "" });
+  const [posts, setPosts] = useState(
+    localStorage.getItem("posts")
+      ? JSON.parse(localStorage.getItem("posts"))
+      : []
+  );
+  localStorage.setItem("posts", JSON.stringify(posts));
+
+  const handleChange = (e) => {
+    setInputValues({
+      ...inputValues,
+      //computed object properties
+      [e.target.name]: e.target.value,
+      id: nanoid(),
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPosts([inputValues, ...posts]);
+  };
+
+  const handleDelete = (id) => {
+    setPosts(
+      posts.filter(p => p.id !== id)
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+      <form onSubmit={handleSubmit}>
+        <input
+          id="title"
+          name="title"
+          value={inputValues.title}
+          onChange={handleChange}
+        />
+        <textarea
+          id="content"
+          name="content"
+          value={inputValues.content}
+          onChange={handleChange}
+        />
+        <button type="submit">Save post</button>
+      </form>
+      <div>
+        {posts.length > 0 &&
+          posts.map((p) => (
+            <div key={p.id}>
+              <h3> {p.title} </h3>
+              <p> {p.content} </p>
+              <p> {p.id} </p>
+              <button onClick={()=>handleDelete(p.id)}>Delete</button>
+            </div>
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
