7ae3372b7667731270fce187c46d669c16aa7172
Brian-Zaniolo <bzaniolo83@gmail.com>
2024-04-11 16:27:18 +0200

    fixed null payload

--- src/store/postsSlice.js
+++ src/store/postsSlice.js
@@ -1,34 +1,40 @@
-import {createSlice, nanoid} from '@reduxjs/toolkit';
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    data: localStorage.getItem('posts') ? JSON.parse(localStorage.getItem('posts')) : []
-}
-
-
+  data: localStorage.getItem("posts")
+    ? JSON.parse(localStorage.getItem("posts"))
+    : [],
+};
 
 const postsSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers: {
-        addPost: {
-            reducer: (state, {payload}) => {
-                state.data = [payload, ...state.data]
-                localStorage.setItem('posts', JSON.stringify(state.data))},
-            prepare: (title, content) => ({
-                title,
-                content,
-                id: nanoid()
-            })
+  name: "posts",
+  initialState,
+  reducers: {
+    addPost: {
+      reducer: (state, { payload }) => {
+        state.data = [payload, ...state.data];
+        localStorage.setItem("posts", JSON.stringify(state.data));
+      },
+      prepare: (title, content) => ({
+        payload: {
+          title,
+          content,
+          id: nanoid(),
+        },
+      }),
     },
-        deletePost:(state, {payload}) =>  {
-            state.data = state.data.filter(p => p.id !== payload);
-            localStorage.setItem('posts', JSON.stringify(state.data))
-        }
+    deletePost: (state, { payload }) => {
+      state.data = state.data.filter((p) => p.id !== payload);
+      localStorage.setItem("posts", JSON.stringify(state.data));
     },
-})
+  },
+});
 
-export const {addPost, deletePost} = postsSlice.actions
+export const { addPost, deletePost } = postsSlice.actions;
 
-export const getAllPosts = s => s.posts.data
+export const getAllPosts = (s) => {
+  console.log(s.posts.data);
+  return s.posts.data;
+};
 
-export default postsSlice.reducer
+export default postsSlice.reducer;
